d9dbd64f970f917c203244d5412834e8b6036305
DayDreaam <dayingyeo@naver.com>
2025-01-15 15:15:49 +0900

    [FEAT] Product 기본 세팅 #9 (#13)

--- backend/src/main/java/com/app/backend/domain/product/entity/Product.java
+++ backend/src/main/java/com/app/backend/domain/product/entity/Product.java
@@ -7,14 +7,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import java.math.BigDecimal;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Products")
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Product extends BaseEntity {
 
     @Id
@@ -22,4 +29,23 @@ public class Product extends BaseEntity {
     @Column(name = "product_id", nullable = false, updatable = false)
     private Long id;
 
+    @Column(name = "product_name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "product_description", nullable = false)
+    private String description;
+
+    @Column(name = "product_image")
+    private String image;
+
+    @Column(name = "product_price", precision = 10, scale = 2, nullable = false)
+    @DecimalMin(value = "100.00", message = "가격은 100원 이상")
+    private BigDecimal price;
+
+    @Column(name = "product_stock")
+    @Min(value=0, message = "재고 수량은 음수 불가")
+    private int stock;
+
+    @Column(name = "product_status")
+    private boolean status;
 }
