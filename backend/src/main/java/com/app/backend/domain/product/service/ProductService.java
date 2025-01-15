33a411a4c98e5b02480f40c38ad9ea15eba55b3d
universechoi <skye.of.universe@gmail.com>
2025-01-15 21:37:13 +0900

    feat: modify product

--- backend/src/main/java/com/app/backend/domain/product/service/ProductService.java
+++ backend/src/main/java/com/app/backend/domain/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.app.backend.domain.product.service;
 
+import com.app.backend.domain.product.controller.ApiV1ProductController;
 import com.app.backend.domain.product.entity.Product;
 import com.app.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +27,27 @@ public class ProductService {
 
     public Optional<Product> findById(long id) {
         return this.productRepository.findById(id);
+    }
+
+    public void modify(Product product, ApiV1ProductController.ModifyProductReqBody modifyProductReqBody) {
+        if (modifyProductReqBody.name() != null) {
+            product.setName(modifyProductReqBody.name());
+        }
+
+        if (modifyProductReqBody.description() != null) {
+            product.setDescription(modifyProductReqBody.description());
+        }
+
+        if (modifyProductReqBody.price() != null) {
+            product.setPrice(modifyProductReqBody.price());
+        }
+
+        if (modifyProductReqBody.amount() != null) {
+            product.setStock(modifyProductReqBody.amount());
+        }
+
+        if (modifyProductReqBody.status() != null) {
+            product.setStatus(modifyProductReqBody.status());
+        }
     }
 }
