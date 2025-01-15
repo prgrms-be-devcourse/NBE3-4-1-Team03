9e0bfb105466ac5d8d01ca66c062dbeb05db118f
universechoi <skye.of.universe@gmail.com>
2025-01-15 21:01:17 +0900

    refactor: primitive type to wrapper class

--- backend/src/main/java/com/app/backend/domain/product/controller/ApiV1ProductController.java
+++ backend/src/main/java/com/app/backend/domain/product/controller/ApiV1ProductController.java
@@ -10,10 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -36,7 +33,7 @@ public class ApiV1ProductController {
 
             @NotNull
             @Min(0)
-            int amount
+            Integer amount
     ){}
 
     @PostMapping
