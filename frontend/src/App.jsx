218f01aefc14a1dd1ed436f08bdd440a6678b313
KangBaekGwa <ksu9801@gmail.com>
2025-01-16 00:05:38 +0900

    feat : 회원가입 컴포넌트 생성 - 입력창 컴포넌트 생성 후 재활용 - 회원가입 후, 로그인 창으로 가도록 라우팅 처리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
 
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <>
@@ -8,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<div>메인페이지 컴포넌트</div>} />
           <Route path="/login" element={<div>로그인 페이지 컴포넌트</div>} />
-          <Route path="/signup" element={<div>회원가입 컴포넌트</div>} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/mypage" element={<div>마이페이지 컴포넌트</div>} />
           <Route path="/admin" element={<div>상품 등록 컴포넌트</div>} />
           <Route path="/logout" element={<div>로그아웃 컴포넌트</div>} />
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,162 @@
+// Signup.js
+import { useNavigate } from "react-router-dom";
+
+import useApi from "../hooks/useApi";
+import useFormValidation from "../hooks/useFormValidation";
+
+import InputField from "../components/InputField";
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const { request, loading } = useApi();
+
+  const validationRules = {
+    username: [
+      (value) => !value && "사용자 이름을 입력해주세요.",
+      (value) =>
+        value.length < 1 ||
+        (value.length > 10 &&
+          "사용자 이름은 1자리 이상, 10자리 이하여야 합니다."),
+    ],
+    email: [
+      (value) => !value && "이메일을 입력해주세요.",
+      (value) => !/\S+@\S+\.\S+/.test(value) && "유효한 이메일을 입력해주세요.",
+    ],
+    password: [
+      (value) => !value && "비밀번호를 입력해주세요.",
+      (value) => value.length < 8 && "비밀번호는 8자리 이상이어야 합니다.",
+      (value) =>
+        !/[!@#$%^&*(),.?":{}|<>]/.test(value) &&
+        "비밀번호는 특수문자를 포함해야 합니다.",
+    ],
+    confirmPassword: [
+      (value, formData) =>
+        value !== formData.password &&
+        "비밀번호와 비밀번호 확인이 일치하지 않습니다.",
+    ],
+    address: [(value) => !value && "주소를 입력해주세요."],
+    address_detail: [(value) => !value && "상세 주소를 입력해주세요."],
+    phone: [
+      (value) => !value && "전화번호를 입력해주세요.",
+      (value) =>
+        !/^\d{10,11}$/.test(value) &&
+        "전화번호는 숫자만 입력해야 하며, 10자리 또는 11자리여야 합니다.",
+    ],
+  };
+
+  const { formData, errors, handleChange, validate } = useFormValidation(
+    {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      address: "",
+      address_detail: "",
+      phone: "",
+    },
+    validationRules
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) return;
+
+    try {
+      const response = await request("/signup", "POST", formData);
+      alert(response.message);
+      navigate("/login");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  return (
+    <section className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex-grow max-w-lg p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+          회원가입
+        </h1>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <InputField
+            id="username"
+            name="username"
+            label="사용자 이름"
+            value={formData.username}
+            onChange={handleChange}
+            error={errors.username}
+            placeholder="사용자 이름을 입력하세요"
+          />
+          <InputField
+            id="email"
+            name="email"
+            label="이메일"
+            value={formData.email}
+            onChange={handleChange}
+            error={errors.email}
+            placeholder="이메일을 입력하세요"
+          />
+          <InputField
+            id="password"
+            name="password"
+            label="비밀번호"
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+            error={errors.password}
+            placeholder="비밀번호를 입력하세요"
+          />
+          <InputField
+            id="confirmPassword"
+            name="confirmPassword"
+            label="비밀번호 확인"
+            type="password"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            error={errors.confirmPassword}
+            placeholder="비밀번호를 다시 입력하세요"
+          />
+          <InputField
+            id="address"
+            name="address"
+            label="주소"
+            value={formData.address}
+            onChange={handleChange}
+            error={errors.address}
+            placeholder="주소를 입력하세요"
+          />
+          <InputField
+            id="address_detail"
+            name="address_detail"
+            label="상세 주소"
+            value={formData.address_detail}
+            onChange={handleChange}
+            error={errors.address_detail}
+            placeholder="상세 주소를 입력하세요"
+          />
+          <InputField
+            id="phone"
+            name="phone"
+            label="전화번호"
+            value={formData.phone}
+            onChange={handleChange}
+            error={errors.phone}
+            placeholder="전화번호를 입력하세요"
+          />
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              disabled={loading}
+            >
+              {loading ? "처리 중..." : "회원가입"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
