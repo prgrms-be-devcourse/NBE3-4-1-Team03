18e000c1aead5122bf5eb3580b5bef8ec4790f69
KangBaekGwa <ksu9801@gmail.com>
2025-01-15 09:29:44 +0900

    style : [FE] 레이아웃 설정 -  추가적으로, 기능단위 라우터 임시 설정 - login, signup, mypage, admin, logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="text-3xl">Hello world</div>
-      </div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<div>메인페이지 컴포넌트</div>} />
+          <Route path="/login" element={<div>로그인 페이지 컴포넌트</div>} />
+          <Route path="/signup" element={<div>회원가입 컴포넌트</div>} />
+          <Route path="/mypage" element={<div>마이페이지 컴포넌트</div>} />
+          <Route path="/admin" element={<div>상품 등록 컴포넌트</div>} />
+          <Route path="/logout" element={<div>로그아웃 컴포넌트</div>} />
+        </Routes>
+      </Layout>
     </>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-600 text-white text-center py-4">
+      <div>문의 사항 : ksu9801@gmail.com</div>
+    </footer>
+  );
+};
+
+export default Footer;
