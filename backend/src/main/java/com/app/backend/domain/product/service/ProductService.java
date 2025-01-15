1e54c1c08a640f0e2be653618f951d1b27626476
universechoi <skye.of.universe@gmail.com>
2025-01-15 20:22:42 +0900

    feat: add product

--- backend/src/main/java/com/app/backend/domain/product/service/ProductService.java
+++ backend/src/main/java/com/app/backend/domain/product/service/ProductService.java
@@ -1,11 +1,25 @@
 package com.app.backend.domain.product.service;
 
+import com.app.backend.domain.product.entity.Product;
 import com.app.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public Product add(String name, String description, BigDecimal price, int amount) {
+        Product product = Product
+                .builder()
+                .name(name)
+                .description(description)
+                .price(price)
+                .stock(amount)
+                .build();
+        return this.productRepository.save(product);
+    }
 }
