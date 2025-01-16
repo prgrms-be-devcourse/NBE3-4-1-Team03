831599fb6c041c3221966fd56528f016ce7ce72a
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-01-16 16:55:31 +0900

    feat: [BE] DomainException 메시지 누락 추가

--- backend/src/main/java/com/app/backend/global/error/exception/DomainException.java
+++ backend/src/main/java/com/app/backend/global/error/exception/DomainException.java
@@ -1,12 +1,20 @@
 package com.app.backend.global.error.exception;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class DomainException extends RuntimeException {
 
     private final ErrorCode errorCode;
+
+    public DomainException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public DomainException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
 
 }
