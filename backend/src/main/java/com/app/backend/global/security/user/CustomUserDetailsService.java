1c8cd44df3da053a70f10f6f8e93e8d259b5d0d9
swjoon <jetkid1446@gmail.com>
2025-01-17 03:42:07 +0900

    feat : Login 기능 일부 구현 - LoginDto 내부에 req와 resp 구현 - DB조회와 계정정보 상태 담는 객체 구현

---
+++ backend/src/main/java/com/app/backend/global/security/user/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.app.backend.global.security.user;
+
+import com.app.backend.domain.user.entity.User;
+import com.app.backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
