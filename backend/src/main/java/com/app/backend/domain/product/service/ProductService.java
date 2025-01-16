17f98c73011da4099d59a85bc73434f2fe489f7d
DayDreaam <dayingyeo@naver.com>
2025-01-16 14:23:41 +0900

    feat 상품 전체 페이지 조회, 단일 상품 조회 구현 #20 (#25)
    상품 조회 기능 추가

---
+++ backend/src/main/java/com/app/backend/domain/product/service/ProductService.java
@@ -0,0 +1,53 @@
+package com.app.backend.domain.product.service;
+
+import com.app.backend.domain.product.entity.Product;
+import com.app.backend.domain.product.exception.ProductException;
+import com.app.backend.domain.product.repository.ProductRepository;
+import com.app.backend.global.error.exception.ErrorCode;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    private static final String CREATED_DATE = "created_date";
+    private static final String PRICE = "price";
+    private static final String NAME = "name";
+
+    public Page<Product> findBySortedPaged(
+            int page, int size, String sort, String direction) {
+        Sort sortOption;
+        // TODO : direction값 검증
+        if(!(direction.equals("asc") || direction.equals("desc"))){
+            throw new ProductException(ErrorCode.PRODUCT_DIRECTION_NOT_EXISTS);
+        }
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        switch (sort) {
+            case CREATED_DATE:
+                sortOption = Sort.by(sortDirection, "createdDate");
+                break;
+            case PRICE:
+                sortOption = Sort.by(sortDirection, "price");
+                break;
+            case NAME:
+                sortOption = Sort.by(sortDirection, "name");
+                break;
+            default:
+                throw new ProductException(ErrorCode.PRODUCT_SORT_NOT_EXISTS);
+        }
+
+        Pageable pageable = PageRequest.of(page, size, sortOption);
+        return productRepository.findAll(pageable);
+    }
+
+    public Optional<Product> findById(Long productId) {
+        return productRepository.findById(productId);
+    }
+}
