be8111d64ee9f89edd346779b145eb75985c9586
swjoon <jetkid1446@gmail.com>
2025-01-17 04:01:53 +0900

    feat : SecurityConfig 추가

---
+++ backend/src/main/java/com/app/backend/global/security/SecurityConfig.java
@@ -0,0 +1,103 @@
+package com.app.backend.global.security;
+
+import com.app.backend.global.security.filter.JwtAuthenticationFilter;
+import com.app.backend.global.security.filter.JwtAuthorizationFilter;
+import com.app.backend.global.security.util.JwtUtil;
+import com.app.backend.global.security.util.AuthResponseUtil;
+import com.app.backend.global.redis.repository.RedisRepository;
+import com.app.backend.global.rs.RsData;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+    private final RedisRepository redisRepository;
+
+    @Value("${jwt.token.access-expiration}")
+    private long ACCESS_EXPIRATION;
+
+    @Value("${jwt.token.refresh-expiration}")
+    private long REFRESH_EXPIRATION;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration configuration) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(
+                jwtUtil, ACCESS_EXPIRATION, REFRESH_EXPIRATION, objectMapper, redisRepository, authenticationManager(configuration));
+        jwtAuthenticationFilter.setFilterProcessesUrl("/api/v1/login");
+
+        JwtAuthorizationFilter jwtAuthorizationFilter = new JwtAuthorizationFilter(
+                jwtUtil, ACCESS_EXPIRATION, REFRESH_EXPIRATION, objectMapper, redisRepository);
+
+        http.headers(head -> head
+                        .frameOptions(option -> option.sameOrigin()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/v1/signup").permitAll()
+                        .requestMatchers("/api/v1/test").authenticated())
+                .addFilterAt(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthorizationFilter, JwtAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) ->
+                        {
+                            AuthResponseUtil.failLogin(
+                                    response, new RsData<>(false, "400", "로그인을 진행해주세요"), HttpServletResponse.SC_UNAUTHORIZED, objectMapper);
+                        }))
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler((request, response, authException) ->
+                        {
+                            AuthResponseUtil.failLogin(
+                                    response, new RsData<>(false, "400", "권한이 없습니다"), HttpServletResponse.SC_UNAUTHORIZED, objectMapper);
+                        }));
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST ..
+        configuration.addAllowedOrigin("*"); // 허용 주소 -> (react 주소 추후 등록 필요)
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization");
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
