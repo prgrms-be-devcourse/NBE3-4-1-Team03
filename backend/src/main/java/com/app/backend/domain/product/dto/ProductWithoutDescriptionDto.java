65e6a4cd3c48f13545588eab499a94dac99e7cd4
daydreaam <dayingyeo@naver.com>
2025-01-16 13:34:38 +0900

    test Product 테스트코드 작성

--- backend/src/main/java/com/app/backend/domain/product/dto/ProductWithoutDescriptionDto.java
+++ backend/src/main/java/com/app/backend/domain/product/dto/ProductWithoutDescriptionDto.java
@@ -8,17 +8,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProductWithoutDescriptionDto {
-    private Long id;
-    private String name;
-    private BigDecimal price;
-    private int stock;
-    private boolean status;
+    private Long product_id;
+    private String product_name;
+    private BigDecimal product_price;
+    private int product_amount;
+    private boolean product_status;
 
     public ProductWithoutDescriptionDto(Product product){
-        this.id = product.getId();
-        this.name = product.getName();
-        this.price = product.getPrice();
-        this.stock = product.getStock();
-        this.status = product.getStatus();
+        this.product_id = product.getId();
+        this.product_name = product.getName();
+        this.product_price = product.getPrice();
+        this.product_amount = product.getStock();
+        this.product_status = product.getStatus();
     }
 }
--- backend/src/main/java/com/app/backend/global/config/AppConfig.java
+++ backend/src/main/java/com/app/backend/global/config/AppConfig.java
@@ -1,28 +1,9 @@
 package com.app.backend.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.activateDefaultTyping(
-                BasicPolymorphicTypeValidator.builder()
-                        .allowIfBaseType(Object.class)
-                        .allowIfSubType(Object.class)
-                        .build(),
-                ObjectMapper.DefaultTyping.JAVA_LANG_OBJECT
-        );
-
-        return objectMapper;
-    }
 
     public static String getSiteFrontUrl() {
         return "http://localhost:3000";
