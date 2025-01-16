99985d6cb78f22ed54d08be707f3c5629897a556
swjoon <jetkid1446@gmail.com>
2025-01-17 03:59:01 +0900

    feat : Jwt 토큰 생성, 응답부 구현

---
+++ backend/src/main/java/com/app/backend/global/security/util/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.app.backend.global.security.util;
+
+import com.app.backend.global.security.user.CustomUserDetails;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS512.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createAccessToken(CustomUserDetails customUserDetails, long expiration) {
+        long currentTime = System.currentTimeMillis();
+
+        return "Bearer " + Jwts.builder()
+                .claim("subject", "access")
+                .claim("username", customUserDetails.getUsername())
+                .claim("role", customUserDetails.getUser().getRole())
+                .issuedAt(new Date(currentTime))
+                .expiration(new Date(currentTime + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String createRefreshToken(CustomUserDetails customUserDetails, long expiration) {
+        long currentTime = System.currentTimeMillis();
+
+        return Jwts.builder()
+                .claim("subject", "refresh")
+                .claim("username", customUserDetails.getUsername())
+                .claim("role", customUserDetails.getUser().getRole())
+                .issuedAt(new Date(currentTime))
+                .expiration(new Date(currentTime + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Cookie setJwtCookie(String key, String value, long expiration) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge((int) expiration / 1000);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+}
