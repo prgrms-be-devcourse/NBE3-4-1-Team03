250cb14bb38570be18562159e544c5d11e0330a6
KangBaekGwa <ksu9801@gmail.com>
2025-01-16 16:01:58 +0900

    feat : API 패칭 로딩/오류 페이지 구현

---
+++ frontend/src/components/Error.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+
+const Error = ({ errorMessage }) => (
+  <div className="flex flex-col items-center justify-center h-full w-full text-center">
+    <div className="text-4xl">⚠️</div>
+    <div className="text-lg mt-2">
+      {errorMessage || "Error! Not Found Page!"}
+    </div>
+  </div>
+);
+
+Error.propTypes = {
+  errorMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.null]),
+};
+
+export default Error;
