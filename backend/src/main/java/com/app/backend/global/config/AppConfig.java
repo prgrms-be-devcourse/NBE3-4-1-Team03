f87b7f77059258708f184f1b8af08145c87b6b65
swjoon <jetkid1446@gmail.com>
2025-01-17 09:11:00 +0900

    remove : 설정된 ObjectMapper 내용 삭제

--- backend/src/main/java/com/app/backend/global/config/AppConfig.java
+++ backend/src/main/java/com/app/backend/global/config/AppConfig.java
@@ -10,15 +10,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.activateDefaultTyping(
-                BasicPolymorphicTypeValidator.builder().allowIfBaseType(Object.class).build(),
-                DefaultTyping.NON_FINAL
-        );
-        return objectMapper;
-    }
-
 }
