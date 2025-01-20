8cc84ff1e9f3582ab01ce30360ef8f257b94c429
swjoon <113027567+swjoon@users.noreply.github.com>
2025-01-20 16:00:30 +0900

    [FEAT]: [BE] Login valid 기능 추가 (#83)

--- backend/src/main/java/com/app/backend/global/security/dto/LoginDto.java
+++ backend/src/main/java/com/app/backend/global/security/dto/LoginDto.java
@@ -1,5 +1,8 @@
 package com.app.backend.global.security.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 public class LoginDto {
@@ -9,7 +12,15 @@ public class LoginDto {
     @AllArgsConstructor
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class LoginRequest {
+
+        @NonNull
+        @NotBlank
+        @Email(message = "올바른 형식의 이메일 주소여야 합니다.")
         private String email;
+
+        @NonNull
+        @NotBlank
+        @Size(min = 8, message = "비밀번호는 최소 8글자 이상이어야 합니다.")
         private String password;
     }
 
